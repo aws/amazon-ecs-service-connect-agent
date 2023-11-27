e8dcb4ee17d3c95a7939ac1ee5d376f0c21e7020
Sunil Narasimhamurthy <sunnrs@amazon.com>
2023-11-27 17:17:03 +0000

    test update since last release

--- agent/envoy_bootstrap/envoy_bootstrap_test.go
+++ agent/envoy_bootstrap/envoy_bootstrap_test.go
@@ -673,7 +673,6 @@ metadata:
     envoy.features.enable_all_deprecated_features: true
     envoy.reloadable_features.http_set_tracing_decision_in_request_id: true
     envoy.reloadable_features.no_extension_lookup_by_name: true
-    envoy.reloadable_features.tcp_pool_idle_timeout: true
     envoy.reloadable_features.sanitize_original_path: true
     envoy.reloadable_features.successful_active_health_check_uneject_host: false
     re2.max_program_size.error_level: 1000
@@ -686,8 +685,6 @@ func TestBuildNodeMetadata_StaticRuntimeMappingDefaultOverridden(t *testing.T) {
 	defer os.Unsetenv("APPMESH_SET_TRACING_DECISION")
 	os.Setenv("ENVOY_NO_EXTENSION_LOOKUP_BY_NAME", "false")
 	defer os.Unsetenv("ENVOY_NO_EXTENSION_LOOKUP_BY_NAME")
-	os.Setenv("ENVOY_ENABLE_TCP_POOL_IDLE_TIMEOUT", "false")
-	defer os.Unsetenv("ENVOY_ENABLE_TCP_POOL_IDLE_TIMEOUT")
 	os.Setenv("ENVOY_SANITIZE_ORIGINAL_PATH", "false")
 	defer os.Unsetenv("ENVOY_SANITIZE_ORIGINAL_PATH")
 	os.Setenv("ENVOY_ACTIVE_HEALTH_CHECK_UNEJECT_HOST", "true")
@@ -705,7 +702,6 @@ metadata:
     envoy.features.enable_all_deprecated_features: true
     envoy.reloadable_features.http_set_tracing_decision_in_request_id: false
     envoy.reloadable_features.no_extension_lookup_by_name: false
-    envoy.reloadable_features.tcp_pool_idle_timeout: false
     envoy.reloadable_features.sanitize_original_path: false
     envoy.reloadable_features.successful_active_health_check_uneject_host: true
     re2.max_program_size.error_level: 1000
@@ -841,7 +837,6 @@ layers:
       envoy.features.enable_all_deprecated_features: true
       envoy.reloadable_features.http_set_tracing_decision_in_request_id: true
       envoy.reloadable_features.no_extension_lookup_by_name: true
-      envoy.reloadable_features.tcp_pool_idle_timeout: true
       envoy.reloadable_features.sanitize_original_path: true
       envoy.reloadable_features.successful_active_health_check_uneject_host: false
       re2.max_program_size.error_level: 1000
@@ -866,7 +861,6 @@ layers:
       envoy.features.enable_all_deprecated_features: true
       envoy.reloadable_features.http_set_tracing_decision_in_request_id: true
       envoy.reloadable_features.no_extension_lookup_by_name: true
-      envoy.reloadable_features.tcp_pool_idle_timeout: true
       envoy.reloadable_features.sanitize_original_path: true
       envoy.reloadable_features.successful_active_health_check_uneject_host: false
       re2.max_program_size.error_level: 1000
